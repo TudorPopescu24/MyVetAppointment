b67f26a734753eec131a5e20965f2ec47236f827
mirunagorea <79169689+mirunagorea@users.noreply.github.com>
2022-11-11 01:52:59 +0200

    DrugStock entity

---
+++ MyVetAppointment.API/MyVetAppointment.API/Entities/DrugStock.cs
@@ -0,0 +1,11 @@
+﻿namespace MyVetAppointment.API.Entities
+{
+    public class DrugStock
+    {
+        public int Id { get; set; }
+
+        public int Quantity { get; set; }
+
+        public ICollection<Drug> Drugs { get; set; }
+    }
+}
