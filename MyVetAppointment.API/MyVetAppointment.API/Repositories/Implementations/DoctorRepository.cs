f4f66afba2b663654fb38f0a4278deeffcf6da7a
TudorPopescu24 <popescutudor66@gmail.com>
2022-11-11 00:24:27 +0200

    Doctor Repository

---
+++ MyVetAppointment.API/MyVetAppointment.API/Repositories/Implementations/DoctorRepository.cs
@@ -0,0 +1,40 @@
+﻿using MyVetAppointment.API.Data;
+using MyVetAppointment.API.Entities;
+using MyVetAppointment.API.Repositories.Interfaces;
+
+namespace MyVetAppointment.API.Repositories.Implementations
+{
+    public class DoctorRepository : IRepository<Doctor>
+    {
+        private readonly MainDbContext _context;
+
+
+        public DoctorRepository(MainDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Doctor doctor)
+        {
+            _context.Doctors.Add(doctor);
+            _context.SaveChanges();
+        }
+
+        public void Update(Doctor doctor)
+        {
+            _context.Doctors.Update(doctor);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var doctor = _context.Doctors.FirstOrDefault(c => c.Id == id);
+
+            if (doctor != null)
+            {
+                _context.Doctors.Remove(doctor);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
