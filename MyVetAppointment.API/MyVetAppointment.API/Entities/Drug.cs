bba6c8e3fac36b8d890852032c766d7f737a68c1
mirunagorea <79169689+mirunagorea@users.noreply.github.com>
2022-11-11 09:08:35 +0200

    Add files via upload

--- MyVetAppointment.API/MyVetAppointment.API/Entities/Drug.cs
+++ MyVetAppointment.API/MyVetAppointment.API/Entities/Drug.cs
@@ -6,6 +6,8 @@
 
         public string Name { get; set; }
 
+        public string Manufacturer { get; set; }
+
         public int Weight { get; set; }
 
         public string Prospect { get; set; }
