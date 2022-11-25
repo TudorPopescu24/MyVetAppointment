c527970425f35a1ffbaeb1701eae3f0009bb3803
Zaharia Mihai-Gabriel <zaharia.mihai.gabriel08@gmail.com>
2022-11-25 15:33:16 +0200

    testing project

---
+++ VetExpert/VetExpert.Testing/TestingWebAppFactory.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VetExpert.Infrastructure;
+
+namespace VetExpert.Testing
+{
+    public class TestingWebAppFactory<TEntryPoint> : WebApplicationFactory<Program> where TEntryPoint : Program
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbContextOptions<MainDbContext>));
+
+                if (descriptor != null)
+                    services.Remove(descriptor);
+
+                services.AddDbContext<MainDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryMainDbTest");
+                });
+
+                var sp = services.BuildServiceProvider();
+                using (var scope = sp.CreateScope())
+                using (var appContext = scope.ServiceProvider.GetRequiredService<MainDbContext>())
+                {
+                    try
+                    {
+                        appContext.Database.EnsureCreated();
+                    }
+                    catch (Exception ex)
+                    {
+                        //Log errors or do anything you think it's needed
+                        throw;
+                    }
+                }
+            });
+        }
+    }
+}
