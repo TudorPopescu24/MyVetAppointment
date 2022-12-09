796643914037a7b04a353ac7e551dfd7c744b592
Zaharia Mihai-Gabriel <zaharia.mihai.gabriel08@gmail.com>
2022-12-09 14:21:21 +0200

    integration tests

---
+++ VetExpert/VetExpert.IntegrationTesting/AppointementControllerTests.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using VetExpert.API.Dto;
+using VetExpert.Domain;
+using VetExpert.Testing;
+using Xunit;
+
+namespace VetExpert.IntegrationTesting
+{
+    [Collection("Database tests")]
+
+    public class AppointmentControllerTests : BaseIntegrationTests, IDisposable
+    {
+        private const string ApiURL = "/api/Appointments/0";
+
+        public AppointmentControllerTests()
+        {
+            CleanDatabases();
+        }
+        [Fact]
+        public async void When_CreatedAppointment_Then_ShouldReturnAppointmentInTheGetRequest()
+        {
+
+            CreateAppointmentDto AppointmentDto = CreateSUT();
+            // Act
+            var createAppointmentResponse = await HttpClient.PostAsJsonAsync(ApiURL, AppointmentDto);
+            var getAppointmentResult = await HttpClient.GetStringAsync("/api/Appointments");
+            // Assert
+            createAppointmentResponse.EnsureSuccessStatusCode();
+            createAppointmentResponse.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+
+
+
+            var Appointments = JsonConvert.DeserializeObject<List<CreateAppointmentDto>>(getAppointmentResult);
+
+            Appointments.Count.Should().Be(1);
+            Appointments.Should().HaveCount(1);
+            Appointments.Should().NotBeNull();
+
+            Dispose();
+        }
+
+        private static CreateAppointmentDto CreateSUT()
+        {
+            // Arrange
+            return new CreateAppointmentDto
+            {
+              DoctorId=Guid.NewGuid(),
+              PetId=Guid.NewGuid()
+
+            };
+        }
+
+        public void Dispose()
+        {
+            CleanDatabases();
+        }
+    }
+}
---
+++ VetExpert/VetExpert.IntegrationTesting/UserControllerTests.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using VetExpert.API.Dto;
+using VetExpert.Testing;
+using Xunit;
+
+namespace VetExpert.IntegrationTesting
+{
+    [Collection("Database tests")]
+    public class UserControllerTests : BaseIntegrationTests, IDisposable
+    {
+        private const string ApiURL = "/api/User";
+
+        public UserControllerTests()
+        {
+            CleanDatabases();
+        }
+        [Fact]
+        public async void When_CreatedUser_Then_ShouldReturnUserInTheGetRequest()
+        { 
+           
+            CreateUserDto userDto = CreateSUT();
+            // Act
+            var createUserResponse = await HttpClient.PostAsJsonAsync(ApiURL, userDto);
+            var getUserResult = await HttpClient.GetStringAsync(ApiURL);
+            // Assert
+            createUserResponse.EnsureSuccessStatusCode();
+            createUserResponse.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+
+
+          
+            var users = JsonConvert.DeserializeObject<List<CreateUserDto>>(getUserResult);
+
+            users.Count.Should().Be(1);
+            users.Should().HaveCount(1);
+            users.Should().NotBeNull();
+
+            Dispose();
+        }
+
+        private static CreateUserDto CreateSUT()
+        {
+            // Arrange
+            return new CreateUserDto
+            {
+                Name = "user 1",
+                Address="my address",
+                Email= "myemail@email",
+                PhoneNumber="***"
+                
+            };
+        }
+
+        public void Dispose()
+        {
+            CleanDatabases();
+        }
+    }
+}
