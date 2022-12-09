301aa660028728bfad5c8473b2545f51f99de61a
IlincaMurarescu <ilincamurarescu@gmail.com>
2022-12-09 11:36:19 +0200

    unit testing

---
+++ VetExpert/VetExpert.Testing/Admins.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VetExpert.Domain;
+
+namespace VetExpert.Testing
+{
+    public class Admins
+    {
+
+        public void AdminValid()
+        {
+            Admin admin = new Admin();
+
+
+
+            Xunit.Assert.NotNull(admin.Id);
+
+        }
+
+
+    }
+}
---
+++ VetExpert/VetExpert.Testing/Clinics.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VetExpert.Domain;
+
+namespace VetExpert.Testing
+{
+    public class Clinics
+    {
+
+        public void ClinicValid()
+        {
+            Clinic clinic = new Clinic();
+
+
+
+            Xunit.Assert.NotNull(clinic.Id);
+
+        }
+
+    }
+}
