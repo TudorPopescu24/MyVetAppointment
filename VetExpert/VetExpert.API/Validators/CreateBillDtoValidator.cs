478c690935bc6102ecb53563094895aa17b9b2db
unknown <gorea_miruna@yahoo.com>
2022-12-22 12:31:00 +0200

    FluentValidation(Admin, Appointment, Bill, Doctor, Drug Dtos)

---
+++ VetExpert/VetExpert.API/Validators/CreateBillDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using VetExpert.API.Dto;
+
+namespace VetExpert.API.Validators
+{
+    public class CreateBillDtoValidator : AbstractValidator<CreateBillDto>
+    {
+        public CreateBillDtoValidator()
+        {
+            RuleFor(x => x.Value).NotNull();
+            RuleFor(x => x.Currency).NotEmpty().NotNull();
+            RuleFor(x => x.DateTime).NotEmpty().NotNull();
+        }
+    }
+}
---
+++ VetExpert/VetExpert.API/Validators/CreateDrugDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using VetExpert.API.Dto;
+
+namespace VetExpert.API.Validators
+{
+    public class CreateDrugDtoValidator : AbstractValidator<CreateDrugDto>
+    {
+        public CreateDrugDtoValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().NotNull();
+            RuleFor(x => x.Manufacturer).NotEmpty().NotNull();
+            RuleFor(x => x.Weight).NotNull();
+            RuleFor(x => x.Prospect).NotEmpty().NotNull();
+            RuleFor(x => x.Price).NotNull();
+        }
+    }
+}
