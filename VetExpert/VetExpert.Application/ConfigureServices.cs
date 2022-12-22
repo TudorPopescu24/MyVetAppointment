f7b1b4fea1cfa56c27ee97580c8ab9f1f4f9b7ef
TudorPopescu24 <popescutudor66@gmail.com>
2022-12-22 14:01:26 +0200

    Resolved IMapper Dependecy

--- VetExpert/VetExpert.Application/ConfigureServices.cs
+++ VetExpert/VetExpert.Application/ConfigureServices.cs
@@ -1,9 +1,6 @@
 ﻿using FluentValidation;
 using FluentValidation.AspNetCore;
 using MediatR;
-using FluentValidation;
-using FluentValidation.AspNetCore;
-using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
