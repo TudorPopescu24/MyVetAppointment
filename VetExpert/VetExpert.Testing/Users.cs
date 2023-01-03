0d1a933be31e4a26d19c863286a2549176309b7d
IlincaMurarescu <ilincamurarescu@gmail.com>
2023-01-03 02:06:39 +0200

    unit testing

--- VetExpert/VetExpert.Testing/Users.cs
+++ VetExpert/VetExpert.Testing/Users.cs
@@ -7,14 +7,18 @@ using VetExpert.Domain;
 
 namespace VetExpert.Testing
 {
+    [TestClass]
     public class Users
     {
+        [TestMethod]
         public void UserValid()
         {
             User user = new User();
 
 
             Assert.AreNotEqual(user.Id, Guid.Empty);
+          //  Assert.AreNotEqual(user.ApplicationUserId, Guid.Empty);
+            Assert.IsNotNull(user.ApplicationUser);
             Assert.IsNotNull(user.Name);
             Assert.IsNotNull(user.Email);
             Assert.IsNotNull(user.Pets);
