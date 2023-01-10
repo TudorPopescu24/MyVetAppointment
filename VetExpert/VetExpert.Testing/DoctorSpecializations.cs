13e8b8bb31abb147853e8cc2589476efe455ebae
IlincaMurarescu <ilincamurarescu@gmail.com>
2023-01-11 00:59:55 +0200

    unit testing coverage improvement

---
+++ VetExpert/VetExpert.Testing/DoctorSpecializations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VetExpert.Domain;
+
+namespace VetExpert.Testing
+{
+    [TestClass]
+    public class DoctorSpecializations
+    {
+        [TestMethod]
+
+        public void DoctorSpecializationValid()
+        {
+            DoctorSpecialization doctspec = new DoctorSpecialization();
+
+            Assert.AreEqual(doctspec.DoctorId, Guid.Empty);
+            Assert.AreEqual(doctspec.SpecializationId, Guid.Empty);
+            Assert.IsNotNull(doctspec.SpecializationId);
+            Assert.IsNotNull(doctspec.Specialization);
+            Assert.IsNotNull(doctspec.Doctor);
+
+
+
+        }
+    }
+}
---
+++ VetExpert/VetExpert.Testing/DrugStocks.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VetExpert.Domain;
+
+namespace VetExpert.Testing
+{
+
+    [TestClass]
+    public class DrugStocks
+    {
+
+        [TestMethod]
+
+        public void DrugStockTest()
+        {
+            DrugStock drugStock= new DrugStock();
+
+            Assert.AreNotEqual(drugStock.Id, Guid.Empty);
+            Assert.AreEqual(drugStock.DrugId, Guid.Empty);
+            Assert.IsNotNull(drugStock.Drug);
+            Assert.IsNotNull(drugStock.ExpirationDate);
+            Assert.AreEqual(drugStock.Quantity, 0);
+
+
+        }
+    }
+}
